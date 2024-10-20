dd30953cfe767163a6b7debe29a8d44d79c6562e
Guinn <eguinjonathan08@gmail.com>
2024-10-20 14:14:40 +0700

    Refactor NavBar component to add minimum width to the background Refactor page component to add maximum width to the container

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -150,7 +150,7 @@ export default function NavBar() {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className="bg-white z-50">
+    <div className="bg-white z-50 min-w-max">
       {/* Mobile menu */}
       <Dialog open={open} onClose={setOpen} className="relative z-50 lg:hidden">
         <DialogBackdrop
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-max flex-col mx-auto">
       <NavBar />
       <CategoryFilter />
     </div>
