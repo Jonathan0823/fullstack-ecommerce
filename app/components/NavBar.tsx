99c063f778dc8103ca3cc867efb083708c347b95
Guinn <eguinjonathan08@gmail.com>
2024-10-20 12:17:40 +0700

    Refactor NavBar component to use Cart component for displaying the shopping cart

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -15,9 +15,10 @@ import {
   TabPanel,
   TabPanels,
 } from '@headlessui/react'
-import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Signinoutbutton from './Signinoutbutton'
 import Image from 'next/image'
+import Cart from './Cart'
 
 
 const navigation = {    
@@ -371,12 +372,9 @@ export default function Example() {
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
                   <a href="#" className="group -m-2 flex items-center p-2">
-                    <ShoppingBagIcon
-                      aria-hidden="true"
-                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                    />
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
-                    <span className="sr-only">items in cart, view bag</span>
+            
+                    <Cart />
+                    
                   </a>
                 </div>
                 <div className="flex flex-1 items-center justify-end space-x-6">
