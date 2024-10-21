bda77ecfb592bf98f3894fd6a4ffbc2bba7be6e2
Guinn <eguinjonathan08@gmail.com>
2024-10-21 18:09:41 +0700

    Refactor admin layout to use getServerSession for authentication

--- app/dashboard/admin/layout.tsx
+++ app/dashboard/admin/layout.tsx
@@ -6,9 +6,7 @@ import { redirect } from "next/navigation";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
-  console.log(session);
   if (!session || !session.user || !session.user.isAdmin) {
-    // Redirect to the login page or an unauthorized page
     redirect("/login");
     return null;
   }
