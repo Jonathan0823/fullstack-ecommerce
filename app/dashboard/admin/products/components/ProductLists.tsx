19771ff0338e1313513521b0b4545ba0c1ae3497
Guinn <eguinjonathan08@gmail.com>
2024-10-21 19:59:16 +0700

    Refactor ProductLists component to display product data in a table with images

--- app/dashboard/admin/products/components/ProductLists.tsx
+++ app/dashboard/admin/products/components/ProductLists.tsx
@@ -1,19 +1,19 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { BACKEND_URL } from "@/lib/constant";
+import Image from "next/image";
 import React from "react";
 
 interface Product {
   id: number;
   name: string;
   price: number;
+  thumbnail: string;
 }
 
 interface ProductListsProps {
@@ -21,6 +21,7 @@ interface ProductListsProps {
 }
 
 const ProductLists: React.FC<ProductListsProps> = ({ products }) => {
+  console.log(products);
   return (
     <div className="bg-white p-5 rounded-xl shadow-md flex-1">
       <p className="font-semibold mb-2 border-b-2 pb-2">Product List</p>
@@ -28,15 +29,26 @@ const ProductLists: React.FC<ProductListsProps> = ({ products }) => {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-80">Name</TableHead>
-            <TableHead>Price</TableHead>
+            <TableHead className="w-28">Image</TableHead>
+            <TableHead className="w-64">Name</TableHead>
+            <TableHead className="w-auto">Price</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {products.map((product: Product) => (
             <TableRow key={product.id}>
+              <TableCell>
+                <Image
+                  src={product.thumbnail}
+                  alt={product.name}
+                  width={80}
+                  height={80}
+                  className="w-20 object-cover"/>
+              </TableCell>
               <TableCell>{product.name}</TableCell>
-              <TableCell>{product.price}</TableCell>
+              <TableCell className="w-2">{product.price}</TableCell>
+              <TableCell className="w-5">edit</TableCell>
+              <TableCell>delete</TableCell>
             </TableRow>
           ))}
         </TableBody>
