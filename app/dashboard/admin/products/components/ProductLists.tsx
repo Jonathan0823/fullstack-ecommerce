0204ca2c38696fc6b0956a24f11ea4073870c33c
Guinn <eguinjonathan08@gmail.com>
2024-10-21 19:39:37 +0700

    Refactor ProductLists component to use Table component for displaying product data

--- app/dashboard/admin/products/components/ProductLists.tsx
+++ app/dashboard/admin/products/components/ProductLists.tsx
@@ -1,5 +1,14 @@
-import { BACKEND_URL } from '@/lib/constant'
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { BACKEND_URL } from "@/lib/constant";
+import React from "react";
 
 interface Product {
   id: number;
@@ -12,18 +21,28 @@ interface ProductListsProps {
 }
 
 const ProductLists: React.FC<ProductListsProps> = ({ products }) => {
-
   return (
-    <div className='bg-white p-5 rounded-xl shadow-md flex-1'>
-      <p className='font-semibold mb-2 border-b-2 pb-2'>Product List</p>
-        {products.map((product: Product) => (
-          <div key={product.id}>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-        ))}
-    </div>
-  )
-}
+    <div className="bg-white p-5 rounded-xl shadow-md flex-1">
+      <p className="font-semibold mb-2 border-b-2 pb-2">Product List</p>
 
-export default ProductLists
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-80">Name</TableHead>
+            <TableHead>Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((product: Product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default ProductLists;
