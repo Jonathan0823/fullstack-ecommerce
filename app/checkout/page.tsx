5b926dc300e8ded96663f29b0a75e1af9c2be8ab
Guinn <eguinjonathan08@gmail.com>
2024-10-21 09:11:28 +0700

    Refactor Products component to fix duplicate product entry in shopping cart

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -28,6 +28,30 @@ const products = [
     imageAlt:
       "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
   },
+  {
+    id: 2,
+    name: "Medium Stuff Satchel",
+    href: "#",
+    color: "Blue",
+    price: "$32.00",
+    quantity: 1,
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+    imageAlt:
+      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
+  },
+  {
+    id: 2,
+    name: "Medium Stuff Satchel",
+    href: "#",
+    color: "Blue",
+    price: "$32.00",
+    quantity: 1,
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+    imageAlt:
+      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
+  },
 ];
 
 const page = () => {
@@ -36,7 +60,7 @@ const page = () => {
     <div>
       <NavBar />
       <div className="flex lg:flex-row-reverse md:flex-col justify-between flex-col">
-        <div className="flex min-h-max min-w-max flex-col overflow-y-scroll bg-white shadow-xl w-full lg:w-2/3 xl:w-1/2" >
+      <div className="flex min-w-max flex-col bg-white shadow-xl w-full lg:w-2/3 xl:w-1/2 lg:max-h-[80vh]">
           <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
             <div className="flex items-start justify-between">
               <p className="text-lg font-medium text-gray-900">Shopping cart</p>
