5d9dcb2d6dd58cb98e22ba801696093147b51af0
Guinn <eguinjonathan08@gmail.com>
2024-10-23 02:11:58 +0700

    Refactor FeaturedSlider component to enable infinite scrolling and autoplay

--- app/components/FeaturedSlider.tsx
+++ app/components/FeaturedSlider.tsx
@@ -26,10 +26,12 @@ export default function FeaturedSlider() {
 
   const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
   return (
     <Slider {...settings} className="rounded-xl">
