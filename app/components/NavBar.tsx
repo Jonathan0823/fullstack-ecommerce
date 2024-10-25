44030548adbcfb76f52280f01be1af59dd3571f6
Guinn <eguinjonathan08@gmail.com>
2024-10-25 10:34:06 +0700

    Refactor NavBar component to remove mobile menu and unnecessary code

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,12 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-} from '@headlessui/react'
-import { Bars3Icon, MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+import {  MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import Signinoutbutton from './Signinoutbutton'
 import Image from 'next/image'
 import Cart from './Cart'
@@ -22,7 +18,6 @@ const navigation = {
 
 
 export default function NavBar() {
-  const [open, setOpen] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
 
   const handleSearch = () => {
@@ -37,63 +32,18 @@ export default function NavBar() {
 
   return (
     <div className="bg-white z-50 ">
-      {/* Mobile menu */}
-      <Dialog open={open} onClose={setOpen} className="relative z-50 lg:hidden">
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-black bg-opacity-25 transition-opacity duration-300 ease-linear data-[closed]:opacity-0 z-50"
-        />
-
-        <div className="fixed inset-0 z-50 flex">
-          <DialogPanel
-            transition
-            className="relative flex w-full max-w-xs transform flex-col overflow-y-auto bg-blue-100 pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:-translate-x-full"
-          >
-            <div className="flex px-4 pb-2 pt-5 z-50">
-              <button
-                type="button"
-                onClick={() => setOpen(false)}
-                className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
-              >
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-              </button>
-            </div>
-
-            
-
-            <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-              {navigation.pages.map((page) => (
-                <div key={page.name} className="flow-root">
-                  <a href={page.href} className="-m-2 block p-2 font-medium text-gray-900">
-                    {page.name}
-                  </a>
-                </div>
-              ))}
-            </div>
-          </DialogPanel>
-        </div>
-      </Dialog>
+      
 
       <header className="relative bg-blue-100">
 
         <nav aria-label="Top" className="max-w-full px-4 sm:px-6 lg:px-8">
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
-              <button
-                type="button"
-                onClick={() => setOpen(true)}
-                className="relative rounded-md bg-white p-2 text-gray-400 lg:hidden"
-              >
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open menu</span>
-                <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-              </button>
+              
               
 
               {/* Logo */}
-              <div className="ml-4 flex lg:ml-0">
+              <div className="mr-2 flex lg:ml-0">
                 <a href="/">
                   <span className="sr-only">Your Company</span>
                   <Image
